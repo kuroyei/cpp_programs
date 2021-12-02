9943be25de26178798172ed2232e4ce8d5b391b9
kuroyei <kroma6317@gmail.com>
2021-12-03 08:01:03 +0900

    cout st, nd, rd, th

---
+++ Ordinal_Number/Ordinal_Numbers.cpp
@@ -0,0 +1,15 @@
+void ordinal(int n){
+	switch(n%10){
+		case: 1
+		      cout << "st";
+		      break;
+		case: 2
+		      cout << "nd";
+		      break;
+		case: 3
+		      cout << "rd";
+		      break;
+		default:
+		      cout << "th";
+	}
+}
---
+++ Ordinal_Number/Ordinal_Numbers.h
@@ -0,0 +1,1 @@
+void ordinal(int n);
