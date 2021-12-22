5a4b3bb5e1fb842ca2637094618bf404f31e7a98
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-12-22 14:35:07 +0900

    Update Ordinal_Numbers.cpp

--- Ordinal_Number/Ordinal_Numbers.cpp
+++ Ordinal_Number/Ordinal_Numbers.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
+using namespace std;
+
 void ordinal(int n){
 	switch(n%10){
-		case: 1
+		case 1:
 		      cout << "st";
 		      break;
-		case: 2
+		case 2:
 		      cout << "nd";
 		      break;
-		case: 3
+		case 3:
 		      cout << "rd";
 		      break;
 		default:
